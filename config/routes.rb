bb35fdb736a9788d9926df8b1866bc0480db4c7b
milenaonmaui <milenakari@yahoo.com>
2019-07-19 12:14:27 -0700

    add cruise resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :cruises
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/signup' => 'users#new'
---
+++ db/migrate/20190719191013_create_cruises.rb
@@ -0,0 +1,14 @@
+class CreateCruises < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cruises do |t|
+      t.string :name
+      t.string :type
+      t.integer :capacity
+      t.integer :price_child
+      t.integer :price_adult
+      t.integer :min_age
+
+      t.timestamps
+    end
+  end
+end
