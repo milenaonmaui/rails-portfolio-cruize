83a3ab8d3883839fe6f95afb53b9bb2a1651aa67
milenaonmaui <milenakari@yahoo.com>
2019-07-22 13:16:51 -0700

    implement cruise index and show

--- app/controllers/cruises_controller.rb
+++ app/controllers/cruises_controller.rb
@@ -1,4 +1,11 @@
 class CruisesController < ApplicationController
 
+    def index
+        @cruises = Cruise.all
+    end
+
+    def show
+        @cruise = Cruise.find_by(:id => params[:id])
+    end
     
 end
