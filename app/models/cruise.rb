23bbd769f4d242866280dd75f3c7fab99d00323b
milenaonmaui <milenakari@yahoo.com>
2019-07-23 12:46:03 -0700

    Add by_category scope to Cruise

--- app/models/cruise.rb
+++ app/models/cruise.rb
@@ -3,4 +3,5 @@ class Cruise < ApplicationRecord
     has_many :bookings
     has_many :users, through: :bookings
     validates :name, presence: true
+    scope :by_category, ->(category_ids) { where(category_id: category_ids) }
 end
