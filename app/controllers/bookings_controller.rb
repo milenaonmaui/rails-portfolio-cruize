57d741a59bac85dbcdffa8b38f0ded492d4ebb94
milenaonmaui <milenakari@yahoo.com>
2019-07-23 18:39:39 -0700

    Add new booking page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,14 @@
 class BookingsController < ApplicationController
+
+    def new
+        if !logged_in?
+            redirect_to login_path
+        end
+        if params[:cruise_id] && @cruise = Cruise.find_by_id(params[:cruise_id])
+            @booking = @cruise.bookings.build
+            @booking.user = current_user
+        else
+            @booking = Booking.new
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :cruises, only: [:show, :index]
-  resources :users do
+  resources :cruises, only: [:show, :index] do
     resources :bookings
   end
+  resources :users 
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/signup' => 'users#new'
   get '/login' => 'sessions#new'
