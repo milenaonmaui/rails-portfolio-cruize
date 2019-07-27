decc2b9c0d85977377272d4f91880c0224d8552d
milenaonmaui <milenakari@yahoo.com>
2019-07-27 14:55:01 -0700

    Fix Booking#seats_left?

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,18 +3,21 @@ class Booking < ApplicationRecord
     belongs_to :cruise
 
     validates :num_adults, :numericality => {greater_than: 0, :message => "At least one adult must be present"}
-    validates :num_children, :numericality => {greater_than_or_equal_to: 0, :message => "Can't be negative"}
+    validates :num_children, :numericality => {greater_than_or_equal_to: 0,  :allow_nil => true, :message => "Can't be negative"}
     validate :seats_available?
 
     def seats_available?
-        if num_adults + num_children > self.cruise.seats_left
+        if (self.num_adults ||=0) + (self.num_children||=0) > self.cruise.seats_left
             errors.add(:base, "Insufficient seats")
+            return false
+        else
+            true
         end
     end
 
     def cost
-        total = num_adults*self.cruise.price_adult
-        total += num_children*self.cruise.price_child if num_children
+        total = self.num_adults*self.cruise.price_adult
+        total += self.num_children*self.cruise.price_child if num_children
         return total
     end
 end
